bd64643e15c142368f51b846e20cb8725f57a726
ElPatronHH <aa.roockie@gmail.com>
2023-12-15 06:37:46 -0600

    last version

--- hotel_proyecto/hotel_app/forms.py
+++ hotel_proyecto/hotel_app/forms.py
@@ -6,6 +6,6 @@ class ReservaForm(forms.ModelForm):
         model = Reserva
         fields = ['habitacion', 'huesped', 'fecha_llegada', 'fecha_salida', 'tipo_pago', 'tipo_habitacion']
         widgets = {
-            'fecha_llegada': forms.TextInput(attrs={'type': 'date'}),  # Utiliza el tipo 'date' para el selector de fecha
-            'fecha_salida': forms.TextInput(attrs={'type': 'date'}),   # Utiliza el tipo 'date' para el selector de fecha
+            'fecha_llegada': forms.TextInput(attrs={'type': 'date'}), 
+            'fecha_salida': forms.TextInput(attrs={'type': 'date'}),
         }
